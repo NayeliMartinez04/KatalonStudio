579bd6a6ce065e61fcafba69e19f5a7c8b17e816
NayeliMartinez04 <nayeliangelicamartinezmoreta@g>
2022-01-28 11:11:53 -0400

    Login email incorrect

---
+++ Object Repository/RecordAndReplay1/Page_Amazon Iniciar sesin/div_Habilita las cookies para continuar    _0980be.rs
@@ -0,0 +1,757 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Habilita las cookies para continuar    _0980be</name>
+   <tag></tag>
+   <elementGuidId>dbe7aa90-eafa-4c7a-a8e6-c8979c84aced</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>#authportal-main-section</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='authportal-main-section']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>authportal-main-section</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>a-section</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+              
+
+
+
+
+
+
+  
+    
+    
+      Habilita las cookies para continuar
+        
+          
+            
+          
+        
+      
+    
+  
+
+
+
+  
+
+
+
+
+
+
+
+
+
+  
+
+    
+      Iniciar sesión
+    
+    
+    
+    
+      nayeliangelicamartinezmoreta@gmail.com
+      
+        Cambiar
+      
+    
+
+    
+    
+
+    
+
+    
+      
+
+      
+      
+
+
+
+
+  
+    
+  
+    
+  
+    
+  
+    
+  
+
+
+      
+        
+        
+        
+        
+        
+
+
+
+
+
+
+  
+    
+      
+        
+          
+            Contraseña
+          
+          
+        
+      
+    
+
+    
+    
+      
+        
+
+
+
+  
+  
+    
+  
+
+
+  ¿Olvidaste tu contraseña?
+
+      
+    
+  
+  
+  
+    
+    
+      
+    
+  
+  
+
+  
+    
+
+
+
+
+  Introduce tu contraseña
+
+
+  
+
+
+  
+
+
+{&quot;sifProfile&quot;:&quot;AuthenticationPortalSigninNA&quot;}
+
+
+
+
+
+        
+        
+
+
+
+
+
+
+
+
+
+        
+          
+          
+            Iniciar sesión
+          
+          
+
+  function cf() {
+    if (typeof window.uet === 'function') {
+      uet('cf');
+    }
+    if (window.embedNotification &amp;&amp;
+      typeof window.embedNotification.onCF === 'function') {
+      embedNotification.onCF();
+    }
+  }
+
+
+cf()
+
+
+          
+          
+
+
+
+
+  
+    
+      
+        
+          Recuérdame.
+          
+            
+              Detalles
+            
+          
+        
+      
+    
+  
+
+
+          
+
+          
+          
+        
+      
+    
+  
+
+
+  
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+            </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;authportal-main-section&quot;)</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//div[@id='authportal-main-section']</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='authportal-center-section']/div</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[@id = 'authportal-main-section' and (text() = concat(&quot;
+              
+
+
+
+
+
+
+  
+    
+    
+      Habilita las cookies para continuar
+        
+          
+            
+          
+        
+      
+    
+  
+
+
+
+  
+
+
+
+
+
+
+
+
+
+  
+
+    
+      Iniciar sesión
+    
+    
+    
+    
+      nayeliangelicamartinezmoreta@gmail.com
+      
+        Cambiar
+      
+    
+
+    
+    
+
+    
+
+    
+      
+
+      
+      
+
+
+
+
+  
+    
+  
+    
+  
+    
+  
+    
+  
+
+
+      
+        
+        
+        
+        
+        
+
+
+
+
+
+
+  
+    
+      
+        
+          
+            Contraseña
+          
+          
+        
+      
+    
+
+    
+    
+      
+        
+
+
+
+  
+  
+    
+  
+
+
+  ¿Olvidaste tu contraseña?
+
+      
+    
+  
+  
+  
+    
+    
+      
+    
+  
+  
+
+  
+    
+
+
+
+
+  Introduce tu contraseña
+
+
+  
+
+
+  
+
+
+{&quot;sifProfile&quot;:&quot;AuthenticationPortalSigninNA&quot;}
+
+
+
+
+
+        
+        
+
+
+
+
+
+
+
+
+
+        
+          
+          
+            Iniciar sesión
+          
+          
+
+  function cf() {
+    if (typeof window.uet === &quot; , &quot;'&quot; , &quot;function&quot; , &quot;'&quot; , &quot;) {
+      uet(&quot; , &quot;'&quot; , &quot;cf&quot; , &quot;'&quot; , &quot;);
+    }
+    if (window.embedNotification &amp;&amp;
+      typeof window.embedNotification.onCF === &quot; , &quot;'&quot; , &quot;function&quot; , &quot;'&quot; , &quot;) {
+      embedNotification.onCF();
+    }
+  }
+
+
+cf()
+
+
+          
+          
+
+
+
+
+  
+    
+      
+        
+          Recuérdame.
+          
+            
+              Detalles
+            
+          
+        
+      
+    
+  
+
+
+          
+
+          
+          
+        
+      
+    
+  
+
+
+  
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+            &quot;) or . = concat(&quot;
+              
+
+
+
+
+
+
+  
+    
+    
+      Habilita las cookies para continuar
+        
+          
+            
+          
+        
+      
+    
+  
+
+
+
+  
+
+
+
+
+
+
+
+
+
+  
+
+    
+      Iniciar sesión
+    
+    
+    
+    
+      nayeliangelicamartinezmoreta@gmail.com
+      
+        Cambiar
+      
+    
+
+    
+    
+
+    
+
+    
+      
+
+      
+      
+
+
+
+
+  
+    
+  
+    
+  
+    
+  
+    
+  
+
+
+      
+        
+        
+        
+        
+        
+
+
+
+
+
+
+  
+    
+      
+        
+          
+            Contraseña
+          
+          
+        
+      
+    
+
+    
+    
+      
+        
+
+
+
+  
+  
+    
+  
+
+
+  ¿Olvidaste tu contraseña?
+
+      
+    
+  
+  
+  
+    
+    
+      
+    
+  
+  
+
+  
+    
+
+
+
+
+  Introduce tu contraseña
+
+
+  
+
+
+  
+
+
+{&quot;sifProfile&quot;:&quot;AuthenticationPortalSigninNA&quot;}
+
+
+
+
+
+        
+        
+
+
+
+
+
+
+
+
+
+        
+          
+          
+            Iniciar sesión
+          
+          
+
+  function cf() {
+    if (typeof window.uet === &quot; , &quot;'&quot; , &quot;function&quot; , &quot;'&quot; , &quot;) {
+      uet(&quot; , &quot;'&quot; , &quot;cf&quot; , &quot;'&quot; , &quot;);
+    }
+    if (window.embedNotification &amp;&amp;
+      typeof window.embedNotification.onCF === &quot; , &quot;'&quot; , &quot;function&quot; , &quot;'&quot; , &quot;) {
+      embedNotification.onCF();
+    }
+  }
+
+
+cf()
+
+
+          
+          
+
+
+
+
+  
+    
+      
+        
+          Recuérdame.
+          
+            
+              Detalles
+            
+          
+        
+      
+    
+  
+
+
+          
+
+          
+          
+        
+      
+    
+  
+
+
+  
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+            &quot;))]</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/RecordAndReplay1/Page_Amazon Iniciar sesin/div_Se produjo un problema                 _c3c363.rs
@@ -0,0 +1,110 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Se produjo un problema                 _c3c363</name>
+   <tag></tag>
+   <elementGuidId>a97b85e2-62b9-40ab-a4e7-477f275ac520</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div.a-box-inner.a-alert-container</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='auth-error-message-box']/div</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>a-box-inner a-alert-container</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Se produjo un problema
+      
+        
+          
+        
+        
+      
+      
+        
+          
+            La contraseña es incorrecta
+          
+        
+      
+    </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;auth-error-message-box&quot;)/div[@class=&quot;a-box-inner a-alert-container&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='auth-error-message-box']/div</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div/div/div/div/div</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = 'Se produjo un problema
+      
+        
+          
+        
+        
+      
+      
+        
+          
+            La contraseña es incorrecta
+          
+        
+      
+    ' or . = 'Se produjo un problema
+      
+        
+          
+        
+        
+      
+      
+        
+          
+            La contraseña es incorrecta
+          
+        
+      
+    ')]</value>
+   </webElementXpaths>
+</WebElementEntity>
